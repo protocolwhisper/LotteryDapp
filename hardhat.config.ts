e9bc63f1ffbff03a6676f234f3a3cc784e522ebb
Theprotocolwhisperer <chrismata10@gmail.com>
2022-10-23 00:38:30 -0500

    Finished smart contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,20 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  networks : {
+    hardhat: {
+      hardfork: "merge"
+    }
+  } , 
+  solidity:{ 
+  version: "0.8.17",
+  settings : {
+    optimizer : {
+      enabled : true , 
+      runs: 1 , 
+    } ,
+  }
+},
 };
 
 export default config;
